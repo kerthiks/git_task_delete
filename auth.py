8f4cfa2bbf2f594acda6d9cabda144a19fb980ed
kerthikdev <kerthikdev@gmail.com>
2025-07-24 22:30:23 +0530

    fix

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 # auth.py
 
 def login(user):
-    if user["role"] == "user":
+    if user["role"] == "kerthik":
         return "User logged in"
     else:
         raise Exception("Only regular users can log in")  # ❌ Bug: excludes admin
